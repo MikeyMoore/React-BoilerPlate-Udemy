9a2410ae6e6eb903e4ec8b6e64bf49f69827e045
MikeyMoore <icarusjedi@gmail.com>
2017-04-11 01:13:54 -0700

    through section 2 lesson 14

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Button from './button';
 
+// with '<Button />', I'm creating an instance, a version, of Button (a React component)
 const App = () => (
   <div>
-    HI
+    <Button name={'Click'} clicked={() => console.log('clicked')} />
+    <Button name={'Search'} />
   </div>
 );
 
